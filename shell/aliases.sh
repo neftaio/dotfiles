a57c9256b94ae87b4af95a1663cdcda6b103ccf4
Neftali Forero <neftaiof@gmail.com>
2020-09-13 05:28:10 +0000

    Comment alias to open visual studio code

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -26,7 +26,7 @@ alias gl="dot git pretty-log"
 alias up="dot package update_all"
 alias copy='pbcopy'
 alias dc='dot docker connect'
-alias vcode='/usr/local/bin/code'
+#alias vcode='/usr/local/bin/code'
 
 alias i.='(idea $PWD &>/dev/null &)'
 alias c.='(code $PWD &>/dev/null &)'
