197c7d476ac9bf2961a8fe1e1882efee68239811
Neftali Forero <neftaiof@gmail.com>
2021-07-11 01:46:29 -0500

    test

--- scripts/self/utils/update.sh
+++ scripts/self/utils/update.sh
@@ -7,7 +7,8 @@ self_update() {
   cd "$DOTFILES_PATH" || exit
 
   git fetch
-  if [[ "$(project_status)" == "behind" ]]; then
+  status=$(project_status)
+  if [[ "$status" == "behind" ]]; then
     log::note "Needs to pull!"
     git pull && exit 0 || log::error "Failed"
   fi
